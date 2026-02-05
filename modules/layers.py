6e67c3ef4b90948470dc228afb875514101dbadb
Wiciu <97093738+WiciuTbg@users.noreply.github.com>
2026-02-05 17:28:58 +0100

    Update layers.py

--- modules/layers.py
+++ modules/layers.py
@@ -15,20 +15,3 @@ class MergeLayer(torch.nn.Module):
     x = torch.cat([x1, x2], dim=1)
     h = self.act(self.fc1(x))
     return self.fc2(h)
-
-
-class MLP(torch.nn.Module):
-  def __init__(self, dim, drop=0.3):
-    super().__init__()
-    self.fc_1 = torch.nn.Linear(dim, 80)
-    self.fc_2 = torch.nn.Linear(80, 10)
-    self.fc_3 = torch.nn.Linear(10, 1)
-    self.act = torch.nn.ReLU()
-    self.dropout = torch.nn.Dropout(p=drop, inplace=False)
-
-  def forward(self, x):
-    x = self.act(self.fc_1(x))
-    x = self.dropout(x)
-    x = self.act(self.fc_2(x))
-    x = self.dropout(x)
-    return self.fc_3(x).squeeze(dim=1)
