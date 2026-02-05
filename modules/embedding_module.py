f658219ead710585834f3f226c779ffe81f3636d
Wiciu <97093738+WiciuTbg@users.noreply.github.com>
2026-02-05 18:15:22 +0100

    Update embedding_module.py

--- modules/embedding_module.py
+++ modules/embedding_module.py
@@ -3,7 +3,7 @@ from torch import nn
 import numpy as np
 import math
 
-from model.temporal_attention import TemporalAttentionLayer
+from modules.temporal_attention import TemporalAttentionLayer
 
 
 
