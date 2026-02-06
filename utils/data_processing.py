0a0f37cf199be48d16f842fef54568e8085adffa
Wiciu <97093738+WiciuTbg@users.noreply.github.com>
2026-02-06 18:45:33 +0100

    Update data_processing.py

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -16,8 +16,8 @@ class Data:
 
 
 def get_data(dataset_name, new_nodes_mode="original implementation"):
-  graph_df = pd.read_csv('./data/ml_{}.csv'.format(dataset_name))
-  edge_features = np.load('./data/ml_{}.npy'.format(dataset_name))
+  graph_df = pd.read_csv(f'./data/ml_{dataset_name}.csv')
+  edge_features = np.load(f'./data/ml_{dataset_name}.npy')
 
   val_time, test_time = list(np.quantile(graph_df.ts, [0.70, 0.85]))
 
@@ -32,6 +32,21 @@ def get_data(dataset_name, new_nodes_mode="original implementation"):
   # Time masks
   val_mask = np.logical_and(timestamps <= test_time, timestamps > val_time)
   test_mask = timestamps > test_time
+
+  # All edges up to (and including) the start of the test split (i.e., train+val in time)
+  pre_test_mask = timestamps <= test_time
+  pre_test_data = Data(
+    sources[pre_test_mask],
+    destinations[pre_test_mask],
+    timestamps[pre_test_mask],
+    edge_idxs[pre_test_mask],
+    labels[pre_test_mask],
+  )
+
+  # Edge features restricted to edges that appear before test_time.
+  # Note: edge_features is indexed by edge_idx; row 0 is usually padding.
+  pre_test_edge_idxs = pre_test_data.edge_idxs
+  edge_features_pre_test = edge_features[pre_test_edge_idxs]
 
   if new_nodes_mode == "original implementation":
     random.seed(2020)
@@ -53,8 +68,10 @@ def get_data(dataset_name, new_nodes_mode="original implementation"):
     observed_edges_mask = np.logical_and(~new_test_source_mask, ~new_test_destination_mask)
 
     train_mask = np.logical_and(timestamps <= val_time, observed_edges_mask)
-    train_data = Data(sources[train_mask], destinations[train_mask], timestamps[train_mask],
-                      edge_idxs[train_mask], labels[train_mask])
+    train_data = Data(
+      sources[train_mask], destinations[train_mask], timestamps[train_mask],
+      edge_idxs[train_mask], labels[train_mask]
+    )
 
     train_node_set = set(train_data.sources) | set(train_data.destinations)
     assert len(train_node_set & new_test_node_set) == 0
@@ -68,8 +85,10 @@ def get_data(dataset_name, new_nodes_mode="original implementation"):
 
   elif new_nodes_mode == "real":
     train_mask = timestamps <= val_time
-    train_data = Data(sources[train_mask], destinations[train_mask], timestamps[train_mask],
-                      edge_idxs[train_mask], labels[train_mask])
+    train_data = Data(
+      sources[train_mask], destinations[train_mask], timestamps[train_mask],
+      edge_idxs[train_mask], labels[train_mask]
+    )
 
     train_nodes = set(train_data.sources) | set(train_data.destinations)
 
@@ -97,22 +116,31 @@ def get_data(dataset_name, new_nodes_mode="original implementation"):
     new_node_test_mask[test_mask] = test_edge_contains_new
 
   else:
-    raise ValueError(f'Unknown new_nodes_mode="{new_nodes_mode}". Use "original implementation" or "real".')
+    raise ValueError(
+      f'Unknown new_nodes_mode="{new_nodes_mode}". Use "original implementation" or "real".'
+    )
 
   # Full val/test sets (all edges)
-  val_data = Data(sources[val_mask], destinations[val_mask], timestamps[val_mask],
-                  edge_idxs[val_mask], labels[val_mask])
-  test_data = Data(sources[test_mask], destinations[test_mask], timestamps[test_mask],
-                   edge_idxs[test_mask], labels[test_mask])
+  val_data = Data(
+    sources[val_mask], destinations[val_mask], timestamps[val_mask],
+    edge_idxs[val_mask], labels[val_mask]
+  )
+  test_data = Data(
+    sources[test_mask], destinations[test_mask], timestamps[test_mask],
+    edge_idxs[test_mask], labels[test_mask]
+  )
 
   # New-node subsets
-  new_node_val_data = Data(sources[new_node_val_mask], destinations[new_node_val_mask],
-                           timestamps[new_node_val_mask], edge_idxs[new_node_val_mask],
-                           labels[new_node_val_mask])
-
-  new_node_test_data = Data(sources[new_node_test_mask], destinations[new_node_test_mask],
-                            timestamps[new_node_test_mask], edge_idxs[new_node_test_mask],
-                            labels[new_node_test_mask])
+  new_node_val_data = Data(
+    sources[new_node_val_mask], destinations[new_node_val_mask],
+    timestamps[new_node_val_mask], edge_idxs[new_node_val_mask],
+    labels[new_node_val_mask]
+  )
+  new_node_test_data = Data(
+    sources[new_node_test_mask], destinations[new_node_test_mask],
+    timestamps[new_node_test_mask], edge_idxs[new_node_test_mask],
+    labels[new_node_test_mask]
+  )
 
   print("The dataset has {} interactions, involving {} different nodes".format(
     full_data.n_interactions, full_data.n_unique_nodes))
@@ -126,9 +154,22 @@ def get_data(dataset_name, new_nodes_mode="original implementation"):
     new_node_val_data.n_interactions, new_node_val_data.n_unique_nodes))
   print("The new node test dataset has {} interactions, involving {} different nodes".format(
     new_node_test_data.n_interactions, new_node_test_data.n_unique_nodes))
+  print("The pre-test dataset has {} interactions, involving {} different nodes".format(
+    pre_test_data.n_interactions, pre_test_data.n_unique_nodes))
 
   if new_nodes_mode == "original implementation":
     print("{} nodes were used for the inductive testing, i.e. are never seen during training".format(
       len(new_test_node_set)))
 
-  return edge_features, full_data, train_data, val_data, test_data, new_node_val_data, new_node_test_data
+  # Return order: features first, then full, then time-based pre-test view, then splits
+  return (
+    edge_features,
+    edge_features_pre_test,
+    full_data,
+    pre_test_data,
+    train_data,
+    val_data,
+    test_data,
+    new_node_val_data,
+    new_node_test_data,
+  )
