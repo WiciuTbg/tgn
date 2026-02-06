013b715dc62f300a27053bfae2975b1501e33fbc
Wiciu <97093738+WiciuTbg@users.noreply.github.com>
2026-02-06 15:48:46 +0100

    Update neighbor_finder.py

--- utils/neighbor_finder.py
+++ utils/neighbor_finder.py
@@ -2,15 +2,14 @@ import numpy as np
 
 
 class NeighborFinder:
-  def __init__(self, data, max_node_idx=None):
+  def __init__(self, data, val=False):
     """
     Build temporal adjacency lists from a Data-like object with fields:
       - sources, destinations, edge_idxs, timestamps (numpy arrays)
     """
-    if max_node_idx == None:
-      self.max_node_idx = int(max(data.sources.max(), data.destinations.max()))
-    else:
-      self.max_node_idx = max_node_idx
+    self.val = val
+    
+    self.max_node_idx = int(max(data.sources.max(), data.destinations.max()))
 
     
     adj_list = [[] for _ in range(self.max_node_idx + 1)]
@@ -50,7 +49,7 @@ class NeighborFinder:
       self.node_to_edge_timestamps[src_idx][:i],
     )
 
-  def get_temporal_neighbor(self, source_nodes, timestamps, n_neighbors=20):
+  def get_temporal_neighbor(self, source_nodes, timestamps, n_neighbors):
     """
     For each (source_node, timestamp) return up to n_neighbors most recent neighbors
     with interaction time < timestamp. Left-pad with zeros if fewer than n_neighbors.
@@ -64,7 +63,7 @@ class NeighborFinder:
     for i, (source_node, timestamp) in enumerate(zip(source_nodes, timestamps)):
       src_neigh, src_eidx, src_ets = self.find_before(int(source_node), float(timestamp))
 
-      if len(src_neigh) > 0 and n_neighbors > 0:
+      if len(src_neigh) > 0:
         src_neigh = src_neigh[-n_neighbors:]
         src_eidx = src_eidx[-n_neighbors:]
         src_ets = src_ets[-n_neighbors:]
@@ -73,4 +72,7 @@ class NeighborFinder:
         neighbors[i, n_neighbors - k:] = src_neigh
         edge_times[i, n_neighbors - k:] = src_ets
         edge_idxs[i, n_neighbors - k:] = src_eidx
+
+        if self.val == True:
+          print(k)
     return neighbors, edge_idxs, edge_times
